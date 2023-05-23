11b300a64040f6310427ae221d5b2690c93d6eb7
tomaslerman <128831511+tomaslerman@users.noreply.github.com>
2023-05-23 08:51:18 -0300

    Add files via upload

---
+++ Models/Pais.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+
+namespace TP4BIS.Models
+{
+public class Pais
+{
+    
+    public string Nombre {get; private set;}
+    public string imgBandera {get; private set;}
+    public int Poblacion {get; private set;}
+    public DateOnly FechaIndependencia{get; private set;}
+    public string AtractivosTuristicos {get; private set;}
+    public string audioHimno{get; private set;}
+
+    public Pais(string nom, string imgBan,int pob, DateOnly F_I, string atrac, string audHim)
+    {
+        Nombre = nom;
+        imgBandera = imgBan;
+        Poblacion = pob;
+        FechaIndependencia = F_I;
+        AtractivosTuristicos = atrac;
+        audioHimno =audHim;
+    }
+}
+
+}
+
